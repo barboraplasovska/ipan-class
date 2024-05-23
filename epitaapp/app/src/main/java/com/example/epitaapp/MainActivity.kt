33dc7217cac507b74866f5e667308236024fb27c
Barbora Plašovská <barbora.plasovska@epita.fr>
2024-05-23 09:24:32 +0200

    feat: epita app

---
+++ epitaapp/app/src/main/java/com/example/epitaapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.epitaapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onClick(view: View?) {
+        val intent = Intent(this, Page2::class.java)
+        intent.putExtra("message", findViewById<EditText>(R.id.input).text.toString())
+        startActivity(intent)
+    }
+}
